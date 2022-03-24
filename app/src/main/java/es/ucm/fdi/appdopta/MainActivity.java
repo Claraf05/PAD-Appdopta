90bf55a3a643519bb2a5a4efb5c8601d1b797660
20mgut00 <60511273+20mgut00@users.noreply.github.com>
2022-03-24 19:57:42 +0100

    Ficha mascota
    Avance en los datos de la ficha
    
    Co-Authored-By: beleen3103 <56888288+beleen3103@users.noreply.github.com>

--- app/src/main/java/es/ucm/fdi/appdopta/MainActivity.java
+++ app/src/main/java/es/ucm/fdi/appdopta/MainActivity.java
@@ -2,13 +2,22 @@ package es.ucm.fdi.appdopta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, ficha.class);
+        startActivity(intent);
+
+
     }
 }
