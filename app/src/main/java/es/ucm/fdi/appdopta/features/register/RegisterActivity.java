81a3520f76514d98e54dd0743ae078d800da3558
Claraf05 <56109465+Claraf05@users.noreply.github.com>
2022-03-24 13:20:21 +0100

    view de login

---
+++ app/src/main/java/es/ucm/fdi/appdopta/features/register/RegisterActivity.java
@@ -0,0 +1,15 @@
+package es.ucm.fdi.appdopta.features.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import es.ucm.fdi.appdopta.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
