082acc1fb25a84c32a202aa11062f249b9c8d7b8
Claraf05 <56109465+Claraf05@users.noreply.github.com>
2022-03-31 13:47:06 +0200

    arreglando bbdd (en proceso)

--- app/src/main/java/es/ucm/fdi/appdopta/database/AppdoptaDBHelper.java
+++ app/src/main/java/es/ucm/fdi/appdopta/database/AppdoptaDBHelper.java
@@ -5,8 +5,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import es.ucm.fdi.appdopta.database.AppdoptaDatabase.UserTable;
+import es.ucm.fdi.appdopta.database.AppdoptaDatabase.StandardUserTable;
 import es.ucm.fdi.appdopta.database.AppdoptaDatabase.PetTable;
+import es.ucm.fdi.appdopta.database.AppdoptaDatabase.PetOwnerTable;
 
 public class AppdoptaDBHelper extends SQLiteOpenHelper {
 
@@ -16,23 +17,56 @@ public class AppdoptaDBHelper extends SQLiteOpenHelper {
     // If you change the database schema, you must increment the database version.
 
     private static final String CREATE_USER_TABLE =
-            "CREATE TABLE " + UserTable.TABLE_NAME + " (" +
-                    UserTable.USERNAME_C + " TEXT UNIQUE NOT NULL PRIMARY KEY," +
-                    UserTable.PASSWORD_C + " TEXT NOT NULL);";
+            "CREATE TABLE " + StandardUserTable.TABLE_NAME + " (" +
+                    StandardUserTable.ID_C + " INTEGER UNIQUE NOT NULL PRIMARY KEY," +
+                    StandardUserTable.USERNAME_C + " TEXT UNIQUE NOT NULL," +
+                    StandardUserTable.PASSWORD_C + " TEXT NOT NULL," +
+                    StandardUserTable.NAME_C + " TEXT NOT NULL," +
+                    StandardUserTable.PHONE_C + " INTEGER NOT NULL," +
+                    StandardUserTable.EMAIL_C + " TEXT NOT NULL," +
+                    StandardUserTable.OWNER_C + " INTEGER NOT NULL," + //0 o 1 if the user is a pet owner
+                    StandardUserTable.PETS2ADOPT_C + " BLOB);";
+
+
+    //for users with StandardUserTable.OWNER_C = 1;
+    private static final String CREATE_PET_OWNER_TABLE =
+            "CREATE TABLE " + PetOwnerTable.TABLE_NAME + " (" +
+                    PetOwnerTable.ID_C + " INTEGER UNIQUE NOT NULL PRIMARY KEY," +
+                    PetOwnerTable.POSTCOD_C + "INTEGER NOT NULL);";
+
 
     private static final String CREATE_PET_TABLE =
             "CREATE TABLE " + PetTable.TABLE_NAME + " (" +
-                    PetTable.ID_OWNER_C + " TEXT NOT NULL PRIMARY KEY," +
+                    //INFO OWNER
+                    PetTable.ID_PET_C + " INTEGER UNIQUE NOT NULL PRIMARY KEY," +
+                    PetTable.ID_OWNER_C + " INTEGER NOT NULL," +
+
+                    //INFO PET
                     PetTable.PETNAME_C + " TEXT NOT NULL," +
                     PetTable.GENDER_C + " TEXT NOT NULL," +
+                    PetTable.SPECIES_C + " TEXT NOT NULL," +
                     PetTable.RACE_C + " TEXT NOT NULL," +
-                    PetTable.VACCINATIONS_C + " TEXT NOT NULL," +
-                    PetTable.WEIGHT_C + "TEXT NOT NULL);";
+                    PetTable.BDAY_C + " TEXT NOT NULL," +
+                    PetTable.DESCRIPTION_C + " TEXT NOT NULL," +
+
+                    //TODAS LAS VACUNAS SON INTEGER QUE PUEDEN SER 1 O 0
+                    PetTable.VACC_RABIA_C + " INTEGER NOT NULL," +
+                    PetTable.VACC_PARVOVIRUS_C + " INTEGER NOT NULL," +
+                    PetTable.VACC_MOQUILLO_C + " INTEGER NOT NULL," +
+                    PetTable.VACC_POLIVALENTE_C + " INTEGER NOT NULL," +
+
+                    //INFO CHIP
+                    PetTable.CHIP_NUM_C + " INTEGER UNIQUE NOT NULL," +
+                    PetTable.CHIP_DATE_C + " TEXT NOT NULL," +
+                    PetTable.CHIP_LOCATION_C + "TEXT NOT NULL);";
+
 
     private static final String DELETE_USER_TABLE = "DROP TABLE IF EXISTS " +
-            UserTable.TABLE_NAME;
+            StandardUserTable.TABLE_NAME;
     private static final String DELETE_PET_TABLE = "DROP TABLE IF EXISTS " +
             PetTable.TABLE_NAME;
+    private static final String DELETE_PET_OWNER_TABLE = "DROP TABLE IF EXISTS " +
+            PetOwnerTable.TABLE_NAME;
 
     // creating a constructor for our database handler.
     public AppdoptaDBHelper(Context context) {
@@ -43,22 +77,29 @@ public class AppdoptaDBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_USER_TABLE);
         db.execSQL(CREATE_PET_TABLE);
+        db.execSQL(CREATE_PET_OWNER_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL(DELETE_USER_TABLE);
         db.execSQL(DELETE_PET_TABLE);
+        db.execSQL(DELETE_PET_OWNER_TABLE);
         onCreate(db);
     }
 
-    public boolean insertUserData(String username, String passw){
+    public boolean insertUserData(int id, String username, String password,String name, int phone,String email,int is_owner){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(UserTable.USERNAME_C, username);
-        contentValues.put(UserTable.PASSWORD_C, passw);
+        contentValues.put(StandardUserTable.ID_C, id);
+        contentValues.put(StandardUserTable.USERNAME_C, username);
+        contentValues.put(StandardUserTable.PASSWORD_C, password);
+        contentValues.put(StandardUserTable.NAME_C, name);
+        contentValues.put(StandardUserTable.PHONE_C, phone);
+        contentValues.put(StandardUserTable.EMAIL_C, email);
+        contentValues.put(StandardUserTable.OWNER_C, is_owner);
 
-        long result = db.insert(UserTable.TABLE_NAME, null, contentValues);
+        long result = db.insert(StandardUserTable.TABLE_NAME, null, contentValues);
         if(result == -1) {
             return false;
         }
@@ -67,15 +108,28 @@ public class AppdoptaDBHelper extends SQLiteOpenHelper {
         }
     }
 
-    public boolean insertPetData(String id, String name, String gend, String race, String vacc, String weight) {
+    public boolean insertPetOwnerData(int id_user, int post_code) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(PetTable.ID_OWNER_C, id);
+        contentValues.put(PetOwnerTable.ID_C, id_user);
+        contentValues.put(PetOwnerTable.POSTCOD_C, post_code);
+
+        long result = db.insert(PetTable.TABLE_NAME, null, contentValues);
+        if(result == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public boolean insertPetData(int id, int id_owner, String petname, String gender, String race, String specie, String bday, int vacc_rabia, int vacc_parvovirus, int vacc_moquillo, int vacc_polivalente, int chip_num, int chip_date, int chip_loc) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(PetTable.ID_OWNER_C, blaba);
         contentValues.put(PetTable.PETNAME_C, name);
         contentValues.put(PetTable.GENDER_C, gend);
         contentValues.put(PetTable.RACE_C, race);
-        contentValues.put(PetTable.VACCINATIONS_C, vacc);
-        contentValues.put(PetTable.WEIGHT_C, weight);
 
         long result = db.insert(PetTable.TABLE_NAME, null, contentValues);
         if(result == -1) {
@@ -88,7 +142,7 @@ public class AppdoptaDBHelper extends SQLiteOpenHelper {
 
     public boolean checkUserName(String user){
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("Select * from "+UserTable.TABLE_NAME+" where "+UserTable.USERNAME_C+" = ?", new String[] {user});
+        Cursor cursor = db.rawQuery("Select * from "+StandardUserTable.TABLE_NAME+" where "+StandardUserTable.USERNAME_C+" = ?", new String[] {user});
         if(cursor.getCount() > 0){
             return true;
         }
--- app/src/main/java/es/ucm/fdi/appdopta/database/AppdoptaDatabase.java
+++ app/src/main/java/es/ucm/fdi/appdopta/database/AppdoptaDatabase.java
@@ -9,21 +9,45 @@ public class AppdoptaDatabase {
    //pa poner lo que salga en cada tabla
 
     //tabla users
-    public static class UserTable implements BaseColumns {
-        public static final String TABLE_NAME = "users";
+    public static class StandardUserTable implements BaseColumns {
+        public static final String TABLE_NAME = "stardard_users";
+        public static final String ID_C = "id";
         public static final String USERNAME_C = "username";
         public static final String PASSWORD_C = "password";
+        public static final String NAME_C = "name";
+        public static final String PHONE_C = "phone";
+        public static final String EMAIL_C = "email";
+        public static final String OWNER_C = "is_owner";
+        //buscar como hacer para añadir las mascotas que alguien quiera adoptar
+        public static final String PETS2ADOPT_C = "pets2adopt";
+    }
+
+    //tabla dueños de mascotas
+    public static class PetOwnerTable implements BaseColumns{
+            public static final String TABLE_NAME = "pet_owners";
+            public static final String ID_C = "id_user";
+            public static final String POSTCOD_C = "post_code";
     }
 
     //tabla mascotas
     public static class PetTable implements BaseColumns {
         public static final String TABLE_NAME = "pets";
+        public static final String ID_PET_C = "id_pet";
+        public static final String DESCRIPTION_C = "description";
         public static final String ID_OWNER_C = "id_owner";
         public static final String PETNAME_C = "petname";
-        public static final String GENDER_C = "sex";
+        public static final String GENDER_C = "gender";
         public static final String RACE_C = "race";
-        public static final String VACCINATIONS_C = "vaccinations";
-        public static final String WEIGHT_C = "weight";
-
+        public static final String SPECIES_C = "specie";
+        public static final String BDAY_C = "bday";
+         //una columna por cada tipiode vacuna
+        public static final String VACC_RABIA_C = "vacc_rabia";
+        public static final String VACC_PARVOVIRUS_C = "vacc_parvovirus";
+        public static final String VACC_MOQUILLO_C = "vacc_moquillo";
+        public static final String VACC_POLIVALENTE_C = "vacc_polivalente";
+        //1 columna por dato del chip(mirar la view de ficha
+        public static final String CHIP_NUM_C = "chip_num";
+        public static final String CHIP_DATE_C = "chip_date";
+        public static final String CHIP_LOCATION_C = "chip_location";
     }
 }
