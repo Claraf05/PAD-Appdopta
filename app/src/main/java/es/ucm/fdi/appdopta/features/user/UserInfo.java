434489aa541db1f8d095b7b8a84aaff81b7098ed
Claraf05 <56109465+Claraf05@users.noreply.github.com>
2022-04-20 18:40:00 +0200

    haciedo lo de cambiar datos de usuario

---
+++ app/src/main/java/es/ucm/fdi/appdopta/features/user/UserInfo.java
@@ -0,0 +1,14 @@
+package es.ucm.fdi.appdopta.features.user;
+
+public class UserInfo {
+
+    //TODO
+    private String id;
+    private String username;
+    private String passw;
+    private String phone;
+    private String email;
+
+
+
+}
