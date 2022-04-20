f939a616001bba13e49d012bbc92b6a06f149540
Edu <bryanedu8@gmail.com>
2022-04-20 10:18:47 +0200

    Revert "Merge branch 'Appdopta2.0' of https://github.com/Claraf05/PAD-Appdopta into Appdopta2.0"
    This reverts commit 88417cbd9a00f705e9a0454c3d707472df1087cb, reversing
    changes made to a1198e360341a879de2aa603e729651a5e3acd0e.

--- app/src/main/java/es/ucm/fdi/appdopta/AdapterItemList.java
+++ app/src/main/java/es/ucm/fdi/appdopta/AdapterItemList.java
@@ -1,8 +1,6 @@
 package es.ucm.fdi.appdopta;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,16 +12,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-import es.ucm.fdi.appdopta.database.AppdoptaDBHelper;
-
 public class AdapterItemList extends RecyclerView.Adapter<AdapterItemList.ViewHolderDatos> {
 
-    private ArrayList<Animal> listaDatos;
+    private ArrayList<String> listaDatos;
     private LayoutInflater layoutInflater;
     private Context context;
-    AppdoptaDBHelper dbHelper;
 
-    public AdapterItemList(Context c, ArrayList<Animal> listDatos) {
+    public AdapterItemList(Context c, ArrayList<String> listDatos) {
         this.context = c;
         this.layoutInflater = LayoutInflater.from(c);
         this.listaDatos = listDatos;
@@ -41,22 +36,6 @@ public class AdapterItemList extends RecyclerView.Adapter<AdapterItemList.ViewHo
     // Enlaza AdapterItemList con class ViewHolderDatos
     public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
         // AIUDA
-        //ArrayList<Animal> petList = dbHelper.readListPetData();
-        Animal mCurrent = listaDatos.get(position); // Crear clase animla: String->Animal
-        TextView t = holder.petName;
-        t.setText(mCurrent.getName());
-        t = holder.petSpecies;
-        t.setText(mCurrent.getSpecies());
-        t = holder.petLocation;
-        t.setText(mCurrent.getLocation());
-
-        //t.setOnClickListener(new View.OnClickListener(){
-            /*@Override
-            public void onClick(View view) {
-                Intent busqueda = new Intent(Intent.ACTION_VIEW, Uri.parse(mCurrent.getURL()));
-                context.startActivity(busqueda);
-            }*/
-        //});
     }
 
     @Override
@@ -67,14 +46,11 @@ public class AdapterItemList extends RecyclerView.Adapter<AdapterItemList.ViewHo
     public class ViewHolderDatos extends RecyclerView.ViewHolder {
         TextView petName;
         TextView petSpecies;
-        TextView petLocation;
 
         public ViewHolderDatos(View itemView){
             super(itemView);
             petName = itemView.findViewById(R.id.nombreMascota);
             petSpecies = itemView.findViewById(R.id.especieMascota);
-            petLocation = itemView.findViewById(R.id.ubicacionMascota);
-
         }
     }
 }
