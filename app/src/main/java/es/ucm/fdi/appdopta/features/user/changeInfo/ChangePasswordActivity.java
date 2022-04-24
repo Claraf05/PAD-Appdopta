421196050fd1a0985a9269bad034263f053a4efb
Claraf05 <56109465+Claraf05@users.noreply.github.com>
2022-04-24 10:14:53 +0200

    Cambiar usuario y contraseña terminados
    tambien hice algunos arreglos en la view de info de usuario porque el correo electronico se veia cortado

--- app/src/main/java/es/ucm/fdi/appdopta/features/user/changeInfo/ChangePasswordActivity.java
+++ app/src/main/java/es/ucm/fdi/appdopta/features/user/changeInfo/ChangePasswordActivity.java
@@ -3,12 +3,16 @@ package es.ucm.fdi.appdopta.features.user.changeInfo;
 import androidx.appcompat.app.AppCompatActivity;
 import es.ucm.fdi.appdopta.R;
 import es.ucm.fdi.appdopta.database.AppdoptaDBHelper;
+import es.ucm.fdi.appdopta.features.login.LoginActivity;
+import es.ucm.fdi.appdopta.features.register.RegisterActivity;
+import es.ucm.fdi.appdopta.features.user.UserActivity;
 import es.ucm.fdi.appdopta.features.user.UserInfo;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
@@ -18,6 +22,7 @@ public class ChangePasswordActivity extends AppCompatActivity {
     AppdoptaDBHelper dbHelper;
     private Bundle user;
     String userid;
+    UserInfo uwu = new UserInfo();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +38,35 @@ public class ChangePasswordActivity extends AppCompatActivity {
         user = getIntent().getExtras();
         userid = user.getString("userInfo");
 
-        UserInfo uwu = new UserInfo();
         dbHelper.buscarUsuario(uwu, userid);
 
     }
 
     public void change_P(View view){
-        //TODO terminar usando la autenticacion del register de referencia
+        String old = actP.getText().toString();
+        String pass = newP.getText().toString();
+        String rep = repP.getText().toString();
+
+        if(old.equals("") || pass.equals("") || rep.equals("")) {
+            Toast.makeText(ChangePasswordActivity.this, "Rellena todos los campos", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Boolean checkuser = dbHelper.checkUserPassword(uwu.getUsername(), old);
+            if(checkuser == true) {
+                if (pass.equals(rep)) {
+                    dbHelper.updateUserPw(userid, pass);
+                    Toast.makeText(ChangePasswordActivity.this, "Se cambió la contraseña con exito", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+                    intent.putExtra("userInfo", userid);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(ChangePasswordActivity.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+                Toast.makeText(ChangePasswordActivity.this, "Contraseña actual inorrecta", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/es/ucm/fdi/appdopta/features/user/changeInfo/ChangeUserActivity.java
+++ app/src/main/java/es/ucm/fdi/appdopta/features/user/changeInfo/ChangeUserActivity.java
@@ -5,19 +5,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import es.ucm.fdi.appdopta.R;
+import es.ucm.fdi.appdopta.database.AppdoptaDBHelper;
+import es.ucm.fdi.appdopta.features.user.UserActivity;
+import es.ucm.fdi.appdopta.features.user.UserInfo;
 
 public class ChangeUserActivity extends AppCompatActivity {
+
+    EditText actN;
+    EditText newN;
+    EditText repN;
+    AppdoptaDBHelper dbHelper;
+    private Bundle user;
+    String userid;
+    UserInfo uwu = new UserInfo();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_user);
+
+        this.setTitle("Cambiar nombre de usuario");
+        actN = (EditText) findViewById(R.id.actUserName);
+        newN = (EditText) findViewById(R.id.newUserName);
+        repN = (EditText) findViewById(R.id.repNewUserName);
+        dbHelper = new AppdoptaDBHelper(this);
+
+        user = getIntent().getExtras();
+        userid = user.getString("userInfo");
+
+        dbHelper.buscarUsuario(uwu, userid);
     }
 
     public void change(View view){
-        //TODO acuerdate de checkear si el nombre de usuario ya existe con dbhelper.checkuser igual que hago en el register
-        //TODO despues ya llamar al metodo de cambiar nombre de usuario
+       
+        String old = actN.getText().toString();
+        String us = newN.getText().toString();
+        String rep = repN.getText().toString();
+
+        if(old.equals("") || us.equals("") || rep.equals("")) {
+            Toast.makeText(ChangeUserActivity.this, "Rellena todos los campos", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Boolean checkuser = dbHelper.checkUserName(old);
+            if(checkuser == true) {
+                if (us.equals(rep)) {
+                    dbHelper.updateUserName(userid, us);
+                    Toast.makeText(ChangeUserActivity.this, "Se cambió el usuario con exito", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+                    intent.putExtra("userInfo", userid);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(ChangeUserActivity.this, "Los usuarios no coinciden", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+                Toast.makeText(ChangeUserActivity.this, "Nombre de usuario actual inorrecto", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
